0da0eb74a0c3df1b78b95eb23ef4da0f92ead45a
user <mirlankyzyainazik13@gmail.com>
2025-05-24 17:19:08 +0600

    test

---
+++ app.js
@@ -0,0 +1,35 @@
+console.log('hello,world');
+
+let costomer='john'
+let costomerName=costomer;
+let costomer_name=costomer
+let $='12'
+let $num='12'
+let _num -12
+let num312=123
+console.log('Имя=' costomer,'','фамилия=',costomerSurname,'Возраст'age);
+console.log('Имя=${costomer},Фамилия=${costomerSurname},Возраст=${age});
+console.log("OAO \"севеэлектро\"")
+let file='d:\\data\\profile.txt'
+console.log(file)
+let number=123
+let number1=3,14
+console.log(number)
+number=500
+console.log(number)
+let a=5
+let b=10
+
+console.log(a+b '+')
+console.log(a-b '-')
+console.log(a*b '*')
+console.log(a/b '/')
+console.log(a%b '%')
+console.log(5 %2 '5%')
+console.log(4%2 '4%')
+console.log(a**b '**')
+console.log(75774487+'asd%')
+console.log("8475873579" + 213)
+let a=10
+let b=5
+console.log(true && false) ||  true)
