63eef2a472b34538d25833d191784c1a98d1b634
user <mirlankyzyainazik13@gmail.com>
2025-06-02 20:40:11 +0600

    leson-8

---
+++ lesson-8/app/app.js
@@ -0,0 +1,22 @@
+console.log('Hello 8')
+
+function greet(){
+    return greet
+}
+
+greet()
+greet()
+greet()
+greet()
+
+function sum(a,b){
+    return a +b
+}
+
+console.log(sum(1,2))
+console.log(sum(10,20))
+console.log(sum(81,8))
+
+function greet1(name){
+    return console.log('hi $(name) ')
+}
